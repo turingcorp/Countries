3188bee1430ab746575f1e1fa8fa7b1a719f8e10
vauxhall <zero.griffin@gmail.com>
2018-10-31 11:04:28 +0100

    Regroup files in Source
    - Separate Catalog entities and models so it is
    easier to find them by functionality.
    - Add groups for Search and Order.

--- Source/Catalog/Catalog.swift
+++ Source/Catalog/Catalog.swift
@@ -3,6 +3,9 @@ import Foundation
 public class Catalog {
     public weak var delegate:Delegate?
     public internal(set) var countries = [Country]()
+    var search:Search!
+    var order:Order!
+    
     public init() { }
     
     public func load(data:Data) {
